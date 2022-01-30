816b8f4566495c4f09db83302795473e362ef58a
ch-planb <chowdhurymuntasir23@gmail.com>
2022-01-30 07:08:57 -0800

    seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -16,7 +16,7 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        DB::table('admins')->insert([
             'name' =>'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin@gmail.com'),
