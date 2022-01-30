5181df7b52e3331862154805f551d67b39769ef8
ch-planb <chowdhurymuntasir23@gmail.com>
2022-01-30 07:00:23 -0800

    added seeder4

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,9 +17,9 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
+            'name' =>'admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'admin@gmail.com',
         ]);
     }
 }
